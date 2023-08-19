ecdcfdab911f4eeade4257e079ee86e7e3f4d2ce
Ratnakar J <j.ratnakar5@gmail.com>
2023-08-19 12:02:54 -0500

    Add files via upload

---
+++ printvar.sh
@@ -0,0 +1,8 @@
+# Program name: "printVar.sh"
+# Linux shell script program to create and print the value of variables.
+
+country="India"
+year=2021
+
+echo "Country name: $country"
+echo "Year: $year"
